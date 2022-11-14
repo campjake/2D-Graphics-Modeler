6edddea13718618c7e6754876b08fcfe686bcc25
Jake Campbell <campbell.jakelog@gmail.com>
2022-11-13 17:55:14 -0800

    Update main.cpp

--- QT_2DGraphicsModeler/main.cpp
+++ QT_2DGraphicsModeler/main.cpp
@@ -7,8 +7,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     LoginDialog login;
-    w.show();
     login.show();
+    w.show();
+
 
 
     return a.exec();
