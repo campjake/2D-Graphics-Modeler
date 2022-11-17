eb29f50dd6ffe12c701c9c7b30235c82fa476fb0
matthewchin47 <113077011+matthewchin47@users.noreply.github.com>
2022-11-17 14:38:39 -0800

    Adding Logo to window

--- QT_2DGraphicsModeler/contactus.cpp
+++ QT_2DGraphicsModeler/contactus.cpp
@@ -1,5 +1,6 @@
 #include "contactus.h"
-#include "ui_contactus.h"
+#include "ui_contactUs.h"
+#include <QPixmap>
 
 ContactUs::ContactUs() :
     QDialog(nullptr),
@@ -7,9 +8,13 @@ ContactUs::ContactUs() :
 {
     ui->setupUi(this);
     setWindowFlags(Qt::Widget | Qt::MSWindowsFixedSizeDialogHint);
+   // QPixmap pix("C:/Users/chinm/Pictures/qt pic/ShapesRUs.jpg");
+   // int w = ui->label_logo->width();
+    //int h = ui->label_logo->height();
+    //ui->label_logo->setPixmap(pix.scaled(100,100,Qt::KeepAspectRatio));
 }
 
-AboutUs::~AboutUs()
+ContactUs::~ContactUs()
 {
     delete ui;
 }
